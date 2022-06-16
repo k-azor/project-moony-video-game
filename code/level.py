bcf27f21194785e2aa866ca3cb2487a911232421
Kenneth Azor <kazortibah5562@gmail.com>
2022-06-16 21:24:44 +0000

    create environment with test assets

--- code/level.py
+++ code/level.py
@@ -1,4 +1,8 @@
+from re import X
+from settings import *
 import pygame
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -10,6 +14,20 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        # sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+
+                if col == 'x':
+                    Tile((x,y), [self.visible_sprites])
+                if col == 'p':
+                    Player((x,y), [self.visible_sprites])
+
     def run(self):
         # update and draw
-        pass
+        self.visible_sprites.draw(self.display_surface)
