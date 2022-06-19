88c7678f33e272a2a2cb5be35c60f02d924e89fa
Kenneth Azor <kazortibah5562@gmail.com>
2022-06-19 01:40:54 +0000

    moving away from matrices

--- code/level.py
+++ code/level.py
@@ -19,15 +19,18 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
+        # using the array WORLD_MAP
+    #     for row_index, row in enumerate(WORLD_MAP):
+    #         for col_index, col in enumerate(row):
+    #             x = col_index * TILESIZE
+    #             y = row_index * TILESIZE
 
-                if col == 'x':
-                    Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
+    #             if col == 'x':
+    #                 Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
+    #             if col == 'p':
+    #                 self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((400,300), [self.visible_sprites], self.obstacle_sprites)
+
 
     def run(self):
         # update and draw
